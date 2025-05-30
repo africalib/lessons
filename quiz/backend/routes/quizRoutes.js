86bc02a04cacf294522d794dc57add0ad095bff1
Hyeop <lsh6166@gmail.com>
2025-05-30 15:43:03 +0900

    퀴즈 권한 수정

--- quiz/backend/routes/quizRoutes.js
+++ quiz/backend/routes/quizRoutes.js
@@ -1,14 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const quizController = require("../controllers/quizController");
-const { auth } = require('../middleware/authMiddleware');
 
 // 퀴즈 API
-router.get("/quizzes/random", auth, quizController.getRandomQuiz);
-router.post("/quizzes/:id/answers", auth, quizController.submitAnswer);
+router.get("/quizzes/random", quizController.getRandomQuiz);
+router.post("/quizzes/:id/answers", quizController.submitAnswer);
 
 // 사용자 API
-router.get("/users/:userId/answers", auth, quizController.getUserAnswers);
-router.get("/users/:userId/answers/:id", auth, quizController.getAnswerDetail);
+router.get("/users/:userId/answers", quizController.getUserAnswers);
+router.get("/users/:userId/answers/:id", quizController.getAnswerDetail);
 
 module.exports = router;
